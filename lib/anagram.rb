8676bc1ed2095fad76845434ca7314ddf0e06f76
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-22 00:52:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,7 +3,7 @@ class Anagram
   def initialize(word)
     @word = word
   end
-  def match(words) 
+  def match(words)
     @words = words
     @words.find_all {|e|e.chars.sort(&:casecmp).join == @word.chars.sort(&:casecmp).join}
   end
