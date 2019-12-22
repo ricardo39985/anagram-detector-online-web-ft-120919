23dc7fc7a57f641c6b8b165a3b970e351d671697
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-22 00:52:13 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+  def match(words)
+    @words = words
+    @words.find_all {|e|e.chars.sort(&:casecmp).join == @word.chars.sort(&:casecmp).join}
+  end
+end
